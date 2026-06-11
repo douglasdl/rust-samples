0ccda0151faa6d4848cf160841fa4cb1770fa5d5
Douglas Dias Leal <douglas_san@hotmail.com>
2026-06-11 15:42:47 +0900

    feat: add introductory Rust examples including basic prints and comments

---
+++ my_project/src/section1_fundamentals/chapter1_introduction/3_comments.rs
@@ -0,0 +1,4 @@
+fn main() {
+  // This is a single-line comment
+  println!("Hello, world!"); // This is another single-line comment
+}
---
+++ my_project/src/section1_fundamentals/chapter1_introduction/3_comments_2.rs
@@ -0,0 +1,7 @@
+fn main() {
+  /*
+  This is a multi-line comment.
+  It can span multiple lines.
+  */
+  println!("Hello, world!");
+}
